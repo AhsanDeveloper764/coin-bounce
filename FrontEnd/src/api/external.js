a0fc93dbbe6ac08799ffba1eafc656eb7ca258ee
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-22 00:48:16 +0500

    update

--- FrontEnd/src/api/external.js
+++ FrontEnd/src/api/external.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY
 const CRYPTO_API_KEY = import.meta.env.VITE_CRYPTO_API_KEY
-const NEWS_API_ENDPOINT = `https://newsapi.org/v2/everything?q=bitcoin&language=en&apiKey=${NEWS_API_KEY}`;
+// const NEWS_API_ENDPOINT = `https://newsapi.org/v2/everything?q=bitcoin&language=en&apiKey=${NEWS_API_KEY}`;
+const NEWS_API_ENDPOINT = 'https://saurav.tech/NewsAPI/top-headlines/category/business/us.json';
 const CRYPTO_API_ENDPOINT = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&
 page=1&sparkline=false&x_cg_demo_api_key=${CRYPTO_API_KEY}`
 
